4130050f7cc6e09e8477abe51081592aba89c177
Raymond Bloemhard <raymond_bloemhard@hotmail.com>
2021-10-21 17:46:28 +0200

    finished local storage clear function

--- app.js
+++ app.js
@@ -19,14 +19,15 @@ const loadEventListeners = () => {
   // Filter task events
   filter.addEventListener("keyup", filterTasks);
 };
-// Get Tasks from localstorage
+
+// // Get Tasks from localstorage
 const getTasks = () => {
-  let tasks;
+  let tasks = [];
   if (localStorage.getItem("tasks") === null) {
-    tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem("tasks"));
   }
+
   tasks.forEach((task) => {
     //Create li element
     const li = document.createElement("li");
@@ -51,8 +52,9 @@ const getTasks = () => {
 const addTask = (e) => {
   if (taskInput.value === "") {
     alert("Add a task");
-    return false;
+    // return false;
   }
+
   //Create li element
   const li = document.createElement("li");
   // Add class
@@ -73,13 +75,14 @@ const addTask = (e) => {
   storeTaskInLocalStorage(taskInput.value);
   // Clear input
   taskInput.value = "";
+
   e.preventDefault();
 };
+
 //------------------------> Store Task in localstorage function
 const storeTaskInLocalStorage = (task) => {
-  let tasks;
+  let tasks = [];
   if (localStorage.getItem("tasks") === null) {
-    tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem("tasks"));
   }
@@ -91,7 +94,7 @@ const storeTaskInLocalStorage = (task) => {
 const removeTask = (e) => {
   if (e.target.parentElement.classList.contains("delete-item")) {
     console.log("is clicked");
-    if (confirm(`Are your sure?`)) {
+    if (confirm("Are your sure?")) {
       e.target.parentElement.parentElement.remove();
       console.log("is removed");
       //Remove from localstorage
@@ -99,17 +102,36 @@ const removeTask = (e) => {
     }
   }
 };
-//-----------------------> Remove Task from localstorage function
-const removeTaskFromLocalStorage = (taskItem) => {};
+
+// -----------------------> Remove Task from localstorage function
+const removeTaskFromLocalStorage = (taskItem) => {
+  let tasks = [];
+  if (localStorage.getItem("tasks") === null) {
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach((task, index) => {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 // ----------------------> Clear tasks function
 const clearTasks = () => {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+  clearTasksFromLocalStorage();
 };
 
-//-----------------------> Filter tasks function
+//----------------------> Clear Tasks from local storage
+const clearTasksFromLocalStorage = () => {
+  localStorage.clear();
+};
+
+// -----------------------> Filter tasks function
 const filterTasks = (e) => {
   const text = e.target.value.toLowerCase();
 
